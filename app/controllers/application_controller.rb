19a5fc9d243e24cb07409e6cd7d8d4947b77cd8c
Nick Simpson <Nick Simpson>
2021-05-14 12:05:43 +0100

    restrict actions from view layer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  # Methods available to both the views and the controllers
+  helper_method :current_user, :logged_in?
+
+  # (db optimisation) return if not nil or lookup
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # convert method return to boolean
+    !!current_user
+  end
+
 end
